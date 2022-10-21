31d5904f049fea5dabd8a0f4326a267281fc433b
ergouzi955 <87901982+ergouzi955@users.noreply.github.com>
2022-10-21 23:06:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oAJx66JmNvW-I1Y009ANu_VvjgFk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'TkdR2-CvCzsPnt_l_4t5hf_sEWGyDMqljQz063bA_uQ',
+      useTemplateId: 'BJl9FLLH9cwxJMSpYtT9CkONW8jrvFH-FbB7W-l8vNs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-17',
       festivals: [
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '吴二狗', year: '1999', date: '06-06',
+          type: '生日', name: '吴二狗', year: '1995', date: '01-17',
         },
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '05-20',
@@ -44,14 +44,14 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2022-05-20' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+       // { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'TkdR2-CvCzsPnt_l_4t5hf_sEWGyDMqljQz063bA_uQ',
+  CALLBACK_TEMPLATE_ID: 'BJl9FLLH9cwxJMSpYtT9CkONW8jrvFH-FbB7W-l8vNs',
 
   CALLBACK_USERS: [
     {
