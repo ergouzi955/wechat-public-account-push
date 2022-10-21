8873ee59a6939bfc902d0a7b95187de6670819fb
ergouzi955 <87901982+ergouzi955@users.noreply.github.com>
2022-10-21 23:13:43 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -40,11 +40,11 @@ const USER_CONFIG = {
         },
       ],
       // 我们在一起已经有xxxx天了的配置
-      //customizedDateList: [
+      customizedDateList: [
         // 在一起的日子
-        //{ keyword: 'love_day', date: '2022-05-20' },
-        // 结婚纪念日
-       // { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2022-05-20' },
+        结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
