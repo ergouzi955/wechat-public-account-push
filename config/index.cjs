833b1f17a0475e3972c144d4bb6f58202912e501
ergouzi955 <87901982+ergouzi955@users.noreply.github.com>
2022-10-21 22:45:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -14,7 +14,7 @@ const USER_CONFIG = {
   APP_SECRET: '702945fc971a91bb44d848e4a4c54f5f',
 
   PROVINCE: '上海',
-  CITY: '上海',
+  CITY: '青浦',
 
   USERS: [
     {
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oAJx66JmNvW-I1Y009ANu_VvjgFk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'd7Jmaj_i6sJouoTzm3y8U7cYT6YJjGLcjQkRcOxswcQ',
+      useTemplateId: 'TkdR2-CvCzsPnt_l_4t5hf_sEWGyDMqljQz063bA_uQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-17',
       festivals: [
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '吴二狗', year: '1999', date: '06-06',
         },
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '05-20',
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'd7Jmaj_i6sJouoTzm3y8U7cYT6YJjGLcjQkRcOxswcQ',
+  CALLBACK_TEMPLATE_ID: 'TkdR2-CvCzsPnt_l_4t5hf_sEWGyDMqljQz063bA_uQ',
 
   CALLBACK_USERS: [
     {
