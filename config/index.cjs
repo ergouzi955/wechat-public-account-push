53c7c8d44be3d483d64e4127bd703b93923725d7
ergouzi955 <87901982+ergouzi955@users.noreply.github.com>
2022-10-27 20:24:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,36 +13,36 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'mwWJ7T7NujaZUzKjZlMHy07l_F9dQEnCXueNwRlz1Xg',
 
-  PROVINCE: '上海',
-  CITY: '青浦',
+  PROVINCE: '大连',
+  CITY: '大连',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '自己',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oAJx66JmNvW-I1Y009ANu_VvjgFk',
+      id: 'oAJx66G5u2QwuZ_tw5PqoNAQw8o4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'DuuaPoaq1vKrCY9jXrJ1ji2RUi15_ObSIQ_wq1U-iWM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '01-17',
+      horoscopeDate: '10-31',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '吴二狗', year: '1999', date: '04-23',
+          type: '*生日', name: '好弟弟', year: '1999', date: '04-23',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '蔡怼怼', year: '1995', date: '01-17',
+          type: '生日', name: '好姐姐', year: '1995', date: '10-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2022', date: '05-20',
+          type: '节日', name: '相识纪念日', year: '2022', date: '04-05',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-05-20' },
+        { keyword: 'love_day', date: '2022-04-05' },
         //结婚纪念日
         //{ keyword: 'marry_day', date: '2022-09-09' },
       ],
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oAJx66JmNvW-I1Y009ANu_VvjgFk',
+      id: 'oAJx66G5u2QwuZ_tw5PqoNAQw8o4',
     }
   ],
 
